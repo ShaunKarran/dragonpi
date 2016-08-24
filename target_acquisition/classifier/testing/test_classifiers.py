c46e39809137404d378027df455d7e918f8e09bc
Shaun Karran <shaun.karran@gmail.com>
2016-08-25 09:17:31 +1000

    Changes set of test images. Better output from test_classifiers.py Update gitignore

--- target_acquisition/classifier/testing/test_classifiers.py
+++ target_acquisition/classifier/testing/test_classifiers.py
@@ -1,13 +1,12 @@
-# Python libs
+from __future__ import division
+
 import argparse
 import os
 import sys
-import time
+from collections import defaultdict
 
-# Coloured print statements
 from termcolor import colored
 
-# OpenCV
 import cv2
 import imutils
 
@@ -18,35 +17,50 @@ expected_results = {
     "image_03.jpg": {"acid": 1, "misc": 0, "flammable": 0},
     "image_04.jpg": {"acid": 1, "misc": 0, "flammable": 0},
     "image_05.jpg": {"acid": 1, "misc": 0, "flammable": 0},
-    "image_06.jpg": {"acid": 0, "misc": 0, "flammable": 0},
-    "image_07.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_06.jpg": {"acid": 0, "misc": 0, "flammable": 1},
+    "image_07.jpg": {"acid": 0, "misc": 0, "flammable": 1},
     "image_08.jpg": {"acid": 0, "misc": 0, "flammable": 1},
     "image_09.jpg": {"acid": 0, "misc": 0, "flammable": 1},
-    "image_10.jpg": {"acid": 0, "misc": 0, "flammable": 1},
-    "image_11.jpg": {"acid": 0, "misc": 0, "flammable": 1},
-    "image_12.jpg": {"acid": 0, "misc": 0, "flammable": 0},
-    "image_13.jpg": {"acid": 0, "misc": 1, "flammable": 0},
-    "image_14.jpg": {"acid": 0, "misc": 1, "flammable": 0},
-    "image_15.jpg": {"acid": 0, "misc": 1, "flammable": 0},
-    "image_16.jpg": {"acid": 0, "misc": 1, "flammable": 1},
-    "image_17.jpg": {"acid": 0, "misc": 0, "flammable": 0},
-    "image_18.jpg": {"acid": 0, "misc": 0, "flammable": 0},
-    "image_19.jpg": {"acid": 0, "misc": 0, "flammable": 0}
+    "image_10.jpg": {"acid": 0, "misc": 1, "flammable": 0},
+    "image_11.jpg": {"acid": 0, "misc": 1, "flammable": 0},
+    "image_12.jpg": {"acid": 0, "misc": 1, "flammable": 0},
+    "image_13.jpg": {"acid": 0, "misc": 1, "flammable": 1},
+    "image_14.jpg": {"acid": 1, "misc": 1, "flammable": 0},
+    "image_15.jpg": {"acid": 1, "misc": 1, "flammable": 1},
+    "image_16.jpg": {"acid": 1, "misc": 0, "flammable": 1},
+    "image_17.jpg": {"acid": 0, "misc": 0, "flammable": 1},
+    "image_18.jpg": {"acid": 1, "misc": 0, "flammable": 0},
+    "image_19.jpg": {"acid": 0, "misc": 1, "flammable": 0},
+    "image_20.jpg": {"acid": 0, "misc": 1, "flammable": 1},
+    "image_21.jpg": {"acid": 1, "misc": 0, "flammable": 0},
+    "image_22.jpg": {"acid": 1, "misc": 0, "flammable": 0},
+    "image_23.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_24.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_25.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_26.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_27.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_28.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_29.jpg": {"acid": 0, "misc": 0, "flammable": 0},
+    "image_30.jpg": {"acid": 0, "misc": 0, "flammable": 0}
 }
 
 
 def main(args):
     """Main."""
-    start_time = time.time()
-
-    print 'Classifiers to be tested:'
+    print 'Testing:\t',
     cascades = {}
     for file_name in os.listdir('./classifiers'):
-        print '\t{}'.format(file_name)
+        print '{}\t'.format(file_name),
         cascades[file_name[:-4]] = cv2.CascadeClassifier(os.path.join('./classifiers', file_name))
-    print ""
+    print ""  # New line.
 
-    num_passed = 0
+    # Store the total number of times each target appears in the set of test images.
+    number_positives = defaultdict(int)
+    for cascade_name, _ in cascades.iteritems():
+        number_positives[cascade_name] += sum([results[cascade_name] for _, results in expected_results.iteritems()])
+
+    number_detected_positives = defaultdict(int)  # Used to store a count of how many times each target is found.
+
     for file_name in os.listdir('./images'):
         image = cv2.imread(os.path.join('./images', file_name))
         image = imutils.resize(image, width=args.resize_width)
@@ -54,16 +68,19 @@ def main(args):
         # Create a copy here so that the image exist and can be written even when no targets are found.
         image_highlighted = image.copy()
 
-        false_positive = False
-        false_negative = False
-        print '{:.0f}ms: Testing {} -'.format((time.time() - start_time) * 1000, file_name),
+        print '{}\t'.format(file_name),
         for cascade_name, cascade in cascades.iteritems():
             results = cascade.detectMultiScale(image)
 
             if len(results) > expected_results[file_name][cascade_name]:
-                false_positive = True
+                print colored('FALSE POSITIVE\t', 'red'),
             elif len(results) < expected_results[file_name][cascade_name]:
-                false_negative = True
+                print colored('FALSE NEGATIVE\t', 'yellow'),
+            elif len(results) == 0:
+                print colored('TRUE NEGATIVE\t', 'white'),
+            else:
+                print colored('TRUE POSITIVE\t', 'green'),
+                number_detected_positives[cascade_name] += 1
 
             for (x, y, w, h) in results:
                 image_highlighted = cv2.rectangle(image_highlighted, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -75,19 +92,13 @@ def main(args):
                             color=(0, 0, 255),
                             thickness=2)
 
-        if false_positive or false_negative:
-            print colored('FAILED -', 'red'),
-            if false_positive:
-                print colored('False positive', 'red')
-            if false_negative:
-                print colored('False negative', 'yellow')
-        else:
-            print colored('PASSED', 'green')
-            num_passed += 1
+        print ""  # New line.
 
         cv2.imwrite(os.path.join('./output', file_name), image_highlighted)
 
-    print '\nTesting complete. {} out of {} passed.'.format(num_passed, len(os.listdir('./images')))
+    print "\nAccuracy\t",
+    for cascade_name, _ in cascades.iteritems():
+        print "{:.1f}%\t\t".format(number_detected_positives[cascade_name] / number_positives[cascade_name] * 100),
 
 
 if __name__ == '__main__':
