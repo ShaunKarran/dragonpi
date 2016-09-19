f806145b6aa10cf2972dd5b2c0fb88c674cac26b
Shaun Karran <shaun.karran@gmail.com>
2016-09-19 12:26:39 +1000

    Fix run_image_nodes.sh to run without output.

--- target_acquisition/ros_pyscripts/run_image_nodes.sh
+++ target_acquisition/ros_pyscripts/run_image_nodes.sh
@@ -1,6 +1,6 @@
 ROS_SCRIPTS_DIR=dragonpi/target_acquisition/ros_pyscripts
 CLASSIFIERS=($ROS_SCRIPTS_DIR/classifiers/acid.xml $ROS_SCRIPTS_DIR/classifiers/flammable.xml $ROS_SCRIPTS_DIR/classifiers/misc.xml)
 
-python $ROS_SCRIPTS_DIR/camera.py \
-python $ROS_SCRIPTS_DIR/image_processor.py -c $CLASSIFIERS -r 600 \
-python $ROS_SCRIPTS_DIR/image_writer.py -t processed_images -o $ROS_SCRIPTS_DIR/processed_images/ \
+python $ROS_SCRIPTS_DIR/camera.py > $ROS_SCRIPTS_DIR/logs/camera.log & \
+python $ROS_SCRIPTS_DIR/image_processor.py -c $CLASSIFIERS -r 600 > $ROS_SCRIPTS_DIR/logs/image_processor.log & \
+python $ROS_SCRIPTS_DIR/image_writer.py -t processed_images -o $ROS_SCRIPTS_DIR/processed_images/ > $ROS_SCRIPTS_DIR/logs/image_writer.log & \
