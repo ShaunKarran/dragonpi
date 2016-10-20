e94d75dac29bbc4b6df88a28ff531fbc29d45aff
Shaun Karran <shaun.karran@gmail.com>
2016-10-20 11:18:10 +1000

    Remove connect call. Update instructions.

--- target_acquisition/ros_pyscripts/vocaliser.py
+++ target_acquisition/ros_pyscripts/vocaliser.py
@@ -7,15 +7,13 @@ class Vocaliser:
 
     def __init__(self):
         self.engine = pyttsx.init()
-        self.engine.connect('finished-utterance', self.onFinishedUtterance)
+        # Callback to fire once when node start. Used to start pyttsx engine looping.
+        self.sub_once = rospy.Subscriber('start_vocaliser', Empty, self.start_loop)
 
         self.acid_subscriber = rospy.Subscriber('classifiers/acid', Bool, self.acid_callback, queue_size=1)
         self.flammable_subscriber = rospy.Subscriber('classifiers/flammable', Bool,
                                                      self.flammable_callback, queue_size=1)
         self.misc_subscriber = rospy.Subscriber('classifiers/misc', Bool, self.misc_callback, queue_size=1)
-
-        # Callback to fire once when node start. Used to start pyttsx engine looping.
-        self.sub_once = rospy.Subscriber('start_loop', Empty, self.start_loop)
 
     def acid_callback(self, ros_data):
         if ros_data.data:
